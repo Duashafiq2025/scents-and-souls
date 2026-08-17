c84d12fbc52eb75764d4241de96ac55890c8def4
Duashafiq2025 <duashafiq2024@gmail.com>
2026-08-17 06:19:03 -0700

    header and hero section added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,18 +6,13 @@ function Header() {
     <nav className="header">
       <img className="logo" src={logo} alt="Scents & Souls" />
       <div>
-        <a>About </a>
-        <a>Home</a>
-        <a>Collections</a>
-        <a>Contact us</a>
+        <a>Home </a>
+        <a>User portal</a>
+        <a>About us</a>
+        
       </div>
-      <div className="left-section">
-        <div className="search-box">
-          <input type="text" placeholder="Search perfumes..." />
-         <button>🔍</button>
-        </div>
-        <a> ♡</a>
-        <a>🛒</a>
+      <div >
+        <a>🛒 cart</a>
       </div>
     </nav>
   );
